7f5e49a598ac674d7aa19adccb1c74d9a63b273f
hejinoh1014 <hejinoh1014@gmail.com>
2020-12-28 17:07:59 -0800

    Modified Item List

--- app.js
+++ app.js
@@ -5,42 +5,30 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = ["Grocery Shopping", "Review the homework", "Write Diary"];
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.get("/", function(req, res) {
 
+  var options = { weekday: 'long', day: 'numeric', month: 'long'};
   var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-    console.log("Error: Current day is equal to: " + currentDay);
-  }
-  res.render("list", {
-    KindofDay: day
-  }); // for the render it has to have view folder and list.ejs and variable that you're using from html
+  var day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", { KindofDay: day, newListItems: items });
+
+});
+
+app.post("/", function(req, res){
+  var item = req.body.newItem;
+
+  items.push(item);
+
+  res.redirect("/");
 });
 
 
