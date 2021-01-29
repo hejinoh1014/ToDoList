8063a80b84f79e6b608c6d4c38227f2f0ce5791a
hejinoh1014 <hejinoh1014@gmail.com>
2021-01-29 02:53:56 -0800

    add about page

--- app.js
+++ app.js
@@ -42,11 +42,11 @@ app.get("/work", function(req,res){
     res.render("list", {listTitle: "Work List", newListItems: workItems});
 });
 
-app.post("/work", function(req,res){
-  let  item = req.body.newItem;
-  workItems.push(item);
-  res.redirect("/work");
-})
+app.get("/about", function(req, res){
+   // app.set("views", "views/header.ejs");
+   // app.set("views", "footer.ejs");
+   res.render("about");
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
